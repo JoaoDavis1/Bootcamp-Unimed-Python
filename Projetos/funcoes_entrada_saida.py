89dbc47bf6326d96e7915e77c800b4fbb33d6d63
JoaoDavis1 <12102040@aluno.cotemig.com.br>
2023-06-08 15:23:25 -0300

    Adição projeto aula: Funções de Entrada e saída

---
+++ Projetos/funcoes_entrada_saida.py
@@ -0,0 +1,11 @@
+nome = input('Qual é o seu nome: ')
+sobrenome = input('Qual é o seu sobrenome: ')
+
+print(f'Olá {nome} {sobrenome}, seja bem vindo!')
+#end(Adiciona no final) | \n(Quebra de linha)
+print(nome, sobrenome, end="... \n")
+#sep(Separador entre variaveis)
+print(nome, sobrenome, sep="-")
+print(nome, sobrenome, sep="_", end="..")
+
+print(float('a'))
